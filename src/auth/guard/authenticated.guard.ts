7cfa3f2253731deb2d47202c996ddb506f42850f
cocozzang <cocozz503@naver.com>
2024-08-08 04:57:22 +0900

    feat: Add auth guard, role guard

--- src/auth/guard/authenticated.guard.ts
+++ src/auth/guard/authenticated.guard.ts
@@ -4,15 +4,27 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
+import { IS_PUBLIC_KEY } from '../decorator';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
   canActivate(context: ExecutionContext): boolean {
     const req = context.switchToHttp().getRequest() as Request;
 
-    if (!req.isAuthenticated())
-      throw new UnauthorizedException('로그인을 해주세요');
+    const isPublic = this.reflector.getAllAndOverride(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
+    if (!req.isAuthenticated()) {
+      throw new UnauthorizedException('로그인이 필요합니다.');
+    }
 
     return true;
   }
